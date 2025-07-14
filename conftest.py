7e689016adf2bc173d719572c24b55cec0bf3cf3
Dim213456 <145069063+Dim213456@users.noreply.github.com>
2025-07-14 16:53:23 +0300

    qa_tests

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 
+import allure
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture(scope='function')
 def driver():
